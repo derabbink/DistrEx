0f62d325d1965307d2b2b34c2dac90cb3d133207
Maarten Abbink <m.abbink@fugro.nl>
2013-07-23 13:42:41 +0200

    added common project

---
+++ DistrEx.Common/Instruction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DistrEx.Common
+{
+    public delegate TResult Instruction<in TArgument, out TProgress, out TResult>(CancellationToken cancellationToken, Action<TProgress> reportProgress, TArgument argument);
+}
