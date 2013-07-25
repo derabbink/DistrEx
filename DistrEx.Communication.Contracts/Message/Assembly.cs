2facfe58148ee78b0b410ba9edc29f72f6984ba1
Maarten Abbink <m.abbink@fugro.nl>
2013-07-25 10:53:54 +0200

    #9 added assembly manager contracts

---
+++ DistrEx.Communication.Contracts/Message/Assembly.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace DistrEx.Communication.Contracts.Message
+{
+    [MessageContract]
+    public class Assembly
+    {
+        //additional data must be [MessageHeader]s
+        /// <summary>
+        /// the (short) name of the assembly
+        /// </summary>
+        [MessageHeader]
+        public string Name;
+        /// <summary>
+        /// the full (long) name of the assembly
+        /// </summary>
+        [MessageHeader]
+        public string FullName;
+
+        //With a stream, there can be only ONE MessageBodyMember, or else transfer reverts to a buffered strategy
+        /// <summary>
+        /// stream producing the assembly/dll file's bytes
+        /// </summary>
+        [MessageBodyMember]
+        public Stream AssemblyStream;
+    }
+}
---
+++ DistrEx.Communication.Contracts/Service/IAssemblyManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using DistrEx.Communication.Contracts.Message;
+
+namespace DistrEx.Communication.Contracts.Service
+{
+    [ServiceContract(Name = "AssemblyManager", Namespace = "http://fugro.schemas/distrex/service/assemblymanager")]
+    public interface IAssemblyManager
+    {
+        [OperationContract]
+        void AddAssembly(Assembly assembly);
+
+        [OperationContract]
+        void Clear();
+    }
+}
