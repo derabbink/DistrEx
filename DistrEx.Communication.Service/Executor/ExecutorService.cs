e2ea2a4307b714279b03622d26c1ab076c636d6f
Maarten Abbink <m.abbink@fugro.nl>
2013-07-25 11:44:22 +0200

    #9 added executor service implementation from RExec project

---
+++ DistrEx.Communication.Service/Executor/ExecutorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using DistrEx.Communication.Contracts.Data;
+using DistrEx.Communication.Contracts.Service;
+using DistrEx.Plugin;
+
+namespace DistrEx.Communication.Service.Executor
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ExecutorService : IExecutor
+    {
+        private PluginManager _pluginManager;
+
+        public ExecutorService(PluginManager pluginManager)
+        {
+            _pluginManager = pluginManager;
+        }
+
+        public object Execute(Instruction instruction)
+        {
+            return _pluginManager.Execute(instruction.AssemblyName, instruction.FqTypeName, instruction.ActionName);
+        }
+    }
+}
