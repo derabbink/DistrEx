ee17d0334d311360ab8ecfc593ccca9a3bb1ef46
Maarten Abbink <m.abbink@fugro.nl>
2013-07-25 11:41:58 +0200

    #9 added executor contracts

---
+++ DistrEx.Communication.Contracts/Data/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace DistrEx.Communication.Contracts.Data
+{
+    [DataContract(Namespace = "http://fugro.schemas/distrex/data/instruction")]
+    public class Instruction
+    {
+        [DataMember]
+        public string AssemblyName { get; set; }
+
+        [DataMember]
+        public string FqTypeName { get; set; }
+
+        [DataMember]
+        public string ActionName { get; set; }
+    }
+}
---
+++ DistrEx.Communication.Contracts/Service/IExecutor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using DistrEx.Communication.Contracts.Data;
+
+namespace DistrEx.Communication.Contracts.Service
+{
+    [ServiceContract(Name = "Executor", Namespace = "http://fugro.schemas/distrex/service/executor")]
+    public interface IExecutor
+    {
+        [OperationContract]
+        object Execute(Instruction instruction);
+    }
+}
