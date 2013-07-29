fe7aad937643aff412e64307606ad033cdd023ac
Maarten Abbink <m.abbink@fugro.nl>
2013-07-29 10:43:28 +0200

    added empty OnWorker test

---
+++ DistrEx.Coordinator.Test/TargetSpecs/OnWorkerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DistrEx.Coordinator.Test.TargetSpecs
+{
+    [TestFixture]
+    public class OnWorkerTest
+    {
+        #region setup
+        [TestFixtureSetUp]
+        public void SetupWorker()
+        {
+            
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+        #endregion
+
+        #region tests
+        
+        #endregion
+
+        #region teardown
+        [TearDown]
+        public void Teardown()
+        {
+            
+        }
+
+        [TestFixtureTearDown]
+        public void TeardownWorker()
+        {
+            
+        }
+        #endregion
+    }
+}
