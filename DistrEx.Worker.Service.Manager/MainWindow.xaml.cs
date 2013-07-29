910d6ffeea7ead5307a7bf8a0b9a469b9fdd4001
Fasika Assegei <fasalex@gmail.com>
2013-07-29 14:13:44 +0200

    #4 Updated Manager UI ...

--- DistrEx.Worker.Service.Manager/MainWindow.xaml.cs
+++ DistrEx.Worker.Service.Manager/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace DistrEx.Worker.Service.Manager
 {
@@ -11,35 +14,50 @@ namespace DistrEx.Worker.Service.Manager
     public partial class MainWindow : Window
     {
         private const string ServiceName = "Worker";
-        private readonly ServiceController service; 
+        private ServiceController service; 
 
         public MainWindow()
         {
             InitializeComponent();
 
+            InitializeService(); 
+        }
+
+        private void InitializeService()
+        {
             ServiceController serviceController = ServiceController.GetServices().FirstOrDefault(i => i.ServiceName.Equals(ServiceName));
 
             if (serviceController == null)
             {
-                ServiceStatus.Content = "Service is not installed.";
+                UpdateStatus("Not installed");
             }
             else
             {
+                UpdateStatus("Installed"); 
                 service = serviceController;
             }
         }
 
         private void InstallButtonClick(object sender, RoutedEventArgs e)
         {
-            const string commandText = "installutil DistrEx.Worker.Service";
-            RunCommand(commandText);
+            const string commandText = "installutil DistrEx.Worker.Service.exe";
+            if (FileName.Text == String.Empty)
+            {
+                //Output error message 
+                return; 
+            }
+            string command = Path.GetDirectoryName(@FileName.Text) + "\\" + commandText;
+            RunCommand(command);
+
+            InitializeService();
         }
 
         private void StartServiceButtonClick(object sender, RoutedEventArgs e)
         {
-            if (service != null)
+            if (service == null)
             {
-                UpdateStatus("Service is not installed.");
+                UpdateStatus("Not installed");
+                return;
             }
 
             if (service.Status == ServiceControllerStatus.Stopped || service.Status == ServiceControllerStatus.Paused)
@@ -53,9 +71,10 @@ namespace DistrEx.Worker.Service.Manager
 
         private void StopServiceButtonClick(object sender, RoutedEventArgs e)
         {
-            if (service != null)
+            if (service == null)
             {
-                UpdateStatus("Service is not installed.");
+                UpdateStatus("Not installed");
+                return; 
             }
 
             if (service.Status == ServiceControllerStatus.Running)
@@ -79,32 +98,13 @@ namespace DistrEx.Worker.Service.Manager
             }
             else
             {
-                UpdateStatus("Service is not installed.");
+                UpdateStatus("Not installed");
             }
         }
         
         private void WindowsServicesClick(object sender, RoutedEventArgs e)
         {
             const string commandText = "/C mmc.exe services.msc";
-            //var process = new Process();
-            
-            //var startInfo = new ProcessStartInfo
-            //{
-            //    RedirectStandardInput = true, 
-            //    UseShellExecute = false,
-            //    Verb = "runas",
-            //    WindowStyle = ProcessWindowStyle.Normal,
-            //    FileName = "cmd.exe",
-            //    Arguments = commandText
-            //};
-            //process.StartInfo = startInfo;
-            //process.Start();
-            ////var input = process.StandardInput;
-            ////input.Write(commandText);
-            ////input.Close();
-
-            //process.WaitForExit();
-            //process.Close();
             RunCommand(commandText);
         }
 
@@ -113,7 +113,7 @@ namespace DistrEx.Worker.Service.Manager
             //Set service to automatic
         }
 
-        private void RunCommand(string command)
+        private static void RunCommand(string command)
         {
             var process = new Process();
             var startInfo = new ProcessStartInfo
@@ -135,7 +135,28 @@ namespace DistrEx.Worker.Service.Manager
         private void UninstallButtonClick(object sender, RoutedEventArgs e)
         {
             const string commandText = "installutil -u DistrEx.Worker.Service";
-            RunCommand(commandText);
+            if (FileName.Text == String.Empty)
+            {
+                //Output error message 
+                return;
+            }
+            string command = Path.GetDirectoryName(@FileName.Text) + "\\" + commandText;
+            RunCommand(command);
+        }
+
+        private void BrowseDirectoryClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Title = "Select the service file executable",
+            };
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string fileName = openFileDialog.FileName;
+                FileName.Text = fileName;
+            }
         }
     }
 }
