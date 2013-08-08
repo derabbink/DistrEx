47b7add08227eda169df34ade5043e25ec112564
Maarten Abbink <m.abbink@fugro.nl>
2013-08-08 16:07:49 +0200

    started automating builds/nuget release

---
+++ NuGet/settings.ps1
@@ -0,0 +1,6 @@
+#####
+# Settings
+#####
+$corflags_cmd = "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\CorFlags.exe"
+$compiler_cmd = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$compiler_options = "/p:Configuration=Release"
